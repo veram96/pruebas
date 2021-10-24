651746b2a7d3b05fa63a9b35ba3dd512b1a1731e
veram96 <angelvemo@gmail.com>
2021-10-24 00:47:09 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+}
+provider "aws" {
+    profile = "default"
+    region  = "us-west-2"
+}
+
+#VPC
+resource "aws_vpc" "main" {
+    cidr_block = "172.31.0.0/16"
+    tags = {
+        Name = "vera-vpc"
+    }
+}
